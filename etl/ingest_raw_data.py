90cfc73a7e53e8e90857be10a670d9f0a2a056c6
bhaskoro-muthohar <bhaskoro.jr@gmail.com>
2023-08-22 17:38:46 +0700

    Untrack .dta files

--- etl/ingest_raw_data.py
+++ etl/ingest_raw_data.py
@@ -1,28 +1,7 @@
-import duckdb
-import pandas as pd
-from tqdm import tqdm
 import os
-
-def read_raw_data():
-    files = {
-        "DM2019_kepesertaan": "raw_data/DM2019_kepesertaan.dta",
-        "DM2020_kepesertaan": "raw_data/DM2020_kepesertaan.dta",
-        "DM2021_fkrtldxsekunder": "raw_data/DM2021_fkrtldxsekunder.dta",
-        "DM2021_fktpnonkapitasi": "raw_data/DM2021_fktpnonkapitasi.dta",
-        "DM2021_kepesertaan": "raw_data/DM2021_kepesertaan.dta",
-        "DM2021_fkrtl": "raw_data/DM2021_fkrtl.dta",
-    }
-    return {name: pd.read_stata(path) for name, path in files.items()}
-
-def convert_to_parquet(data):
-    parquet_paths = {}
-    for name, df in data.items():
-        for col in df.select_dtypes(['category']):
-            df[col] = df[col].astype(str)
-        parquet_path = f"raw_data/{name}.parquet"
-        df.to_parquet(parquet_path)
-        parquet_paths[name] = parquet_path
-    return parquet_paths
+import duckdb
+from tqdm import tqdm
+import glob
 
 def create_tables(cursor):
     tables = [
@@ -31,6 +10,8 @@ def create_tables(cursor):
         "dm2021_fkrtldxsekunder",
         "dm2021_fktpnonkapitasi",
         "dm2021_kepesertaan",
+        "dm2021_fkrtl",
+        "dm2021_fktpkapitasi"
     ]
     for table in tables:
         cursor.execute(f"DROP TABLE IF EXISTS {table};")
@@ -141,21 +122,107 @@ def create_tables(cursor):
     );
     """)
 
-def ingest_parquet_files(conn, parquet_paths):
+    cursor.execute("""
+    CREATE TABLE dm2021_fkrtl (
+        PSTV01 INTEGER,
+        PSTV02 INTEGER,
+        PSTV15 FLOAT,
+        FKP02 VARCHAR,
+        FKL02 VARCHAR,
+        FKL03 DATETIME,
+        FKL04 DATETIME,
+        FKL05 VARCHAR,
+        FKL06 VARCHAR,
+        FKL07 VARCHAR,
+        FKL08 VARCHAR,
+        FKL09 VARCHAR,
+        FKL10 VARCHAR,
+        FKL11 VARCHAR,
+        FKL12 VARCHAR,
+        FKL13 VARCHAR,
+        FKL14 VARCHAR,
+        FKL15 VARCHAR,
+        FKL15A VARCHAR,
+        FKL16 VARCHAR,
+        FKL16A VARCHAR,
+        FKL17 VARCHAR,
+        FKL17A VARCHAR,
+        FKL18 VARCHAR,
+        FKL18A VARCHAR,
+        FKL19 VARCHAR,
+        FKL19A VARCHAR,
+        FKL20 VARCHAR,
+        FKL21 VARCHAR,
+        FKL22 INTEGER,
+        FKL23 VARCHAR,
+        FKL25 VARCHAR,
+        FKL26 VARCHAR,
+        FKL27 VARCHAR,
+        FKL28 VARCHAR,
+        FKL29 VARCHAR,
+        FKL30 VARCHAR,
+        FKL31 VARCHAR,
+        FKL32 INTEGER,
+        FKL33 VARCHAR,
+        FKL34 INTEGER,
+        FKL35 VARCHAR,
+        FKL36 VARCHAR,
+        FKL37 INTEGER,
+        FKL38 VARCHAR,
+        FKL39 VARCHAR,
+        FKL40 INTEGER,
+        FKL41 VARCHAR,
+        FKL42 VARCHAR,
+        FKL43 INTEGER,
+        FKL44 VARCHAR,
+        FKL45 VARCHAR,
+        FKL46 INTEGER,
+        FKL47 INTEGER,
+        FKL48 INTEGER,
+    );
+    """)
+
+    cursor.execute("""
+    CREATE TABLE dm2021_fktpkapitasi (
+        PSTV01 INTEGER,
+        PSTV02 INTEGER,
+        PSTV15 FLOAT,
+        FKP02 VARCHAR,
+        FKP03 DATETIME,
+        FKP04 DATETIME,
+        FKP05 INTEGER,
+        FKP06 INTEGER,
+        FKP07 INTEGER,
+        FKP08 INTEGER,
+        FKP09 INTEGER,
+        FKP10 INTEGER,
+        FKP11 INTEGER,
+        FKP12 INTEGER,
+        FKP13 INTEGER,
+        FKP14 INTEGER,
+        FKP14A VARCHAR,
+        FKP15 VARCHAR,
+        FKP15A VARCHAR,
+        FKP16 INTEGER,
+        FKP17 INTEGER,
+        FKP18 INTEGER,
+        FKP19 INTEGER,
+        FKP20 INTEGER,
+        FKP21 INTEGER,
+        FKP22 INTEGER,
+    );
+    """)
+
+def ingest_parquet_files(conn):
+    parquet_paths = glob.glob("raw_data/*.parquet")
     cursor = conn.cursor()
-    for name, path in tqdm(parquet_paths.items(), desc="Ingesting Parquet files"):
-        table_name = name.lower()
+    for path in tqdm(parquet_paths, desc="Ingesting Parquet files"):
+        table_name = os.path.basename(path).split('.')[0].lower()
         cursor.execute(f"COPY {table_name} FROM '{path}' (FORMAT 'parquet');")
         conn.commit()
     cursor.close()
 
 def main():
-    # Read raw data
-    data = read_raw_data()
-
-    # Convert to Parquet
-    parquet_paths = convert_to_parquet(data)
-
     # Connect to DuckDB
     conn = duckdb.connect('oeroenremboog.db')
     cursor = conn.cursor()
@@ -164,7 +231,7 @@ def main():
     create_tables(cursor)
 
     # Ingest Parquet files into DuckDB
-    ingest_parquet_files(conn, parquet_paths)
+    ingest_parquet_files(conn)
 
     # Close connection
     cursor.close()
