d2ceb54a6a8f3e503fa9bab59a1694b0494088fb
bhaskoro-muthohar <bhaskoro.jr@gmail.com>
2023-08-26 16:44:22 +0700

    major changes on repo structure and add frontend functionality

--- app.py
+++ app.py
@@ -1,108 +1,36 @@
 import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import geopandas as gpd
-import plotly.graph_objects as go
-import random
+import streamlit.components.v1 as components
 from streamlit_option_menu import option_menu
-from database_config import connect_to_db
 
-def fetch_data():
-    conn = connect_to_db()
-    query = "SELECT * FROM dm2021_kepesertaan"
-    result = conn.execute(query).fetch_df()
-    conn.close()
-    return result
+from src.pages import data_view, distribution_chart, correlation_heatmap, interactive_map, table_view, welcome_page
 
-def data_view():
-    st.title('Oeroenremboog Data Visualization')
-    st.subheader('Data from dm2021_kepesertaan table')
-    data = fetch_data()
-    st.write(data)
+st.set_page_config(
+    page_title="Oeroenremboog Dashboard",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
-def distribution_chart():
-    st.title('Distribution Chart')
-    mock_data = pd.DataFrame({'Value': [50, 60, 70, 80, 90]})
-    st.bar_chart(mock_data['Value'])
-
-def correlation_heatmap():
-    st.title('Correlation Heatmap')
-    mock_data = pd.DataFrame({'A': [1, 2, 3], 'B': [2, 3, 4], 'C': [3, 4, 5]})
-    corr = mock_data.corr()
-
-    fig, ax = plt.subplots()
-
-    sns.heatmap(corr, annot=True, ax=ax)
-
-    st.pyplot(fig)
-
-def interactive_map():
-    st.title('Interactive Map of Indonesia Provinces')
-
-    indonesia_map = gpd.read_file('raw_data/indonesia-prov.geojson')
-
-    indonesia_map = indonesia_map.to_crs("EPSG:3395")
-
-    indonesia_map['geometry'] = indonesia_map['geometry'].centroid
-
-    indonesia_map = indonesia_map.to_crs("EPSG:4326")
-
-    indonesia_map['latitude'] = indonesia_map['geometry'].y
-    indonesia_map['longitude'] = indonesia_map['geometry'].x
-
-    indonesia_map['Mock Value'] = [random.randint(100, 1000) for _ in range(len(indonesia_map))]
-
-    fig = go.Figure(data=go.Scattergeo(
-        lon=indonesia_map['longitude'],
-        lat=indonesia_map['latitude'],
-        text=indonesia_map['Propinsi'] + ': ' + indonesia_map['Mock Value'].astype(str),
-        mode='markers',
-        marker=dict(size=12, color=indonesia_map['Mock Value'], colorscale='Viridis')
-    ))
-
-    fig.update_layout(
-        geo=dict(
-            scope='asia',
-            projection_type='mercator',
-            showland=True,
-            landcolor='rgb(243, 243, 243)',
-            countrycolor='rgb(204, 204, 204)',
-        ),
-        margin={"r": 0, "t": 0, "l": 0, "b": 0}
-    )
-
-    st.plotly_chart(fig)
-
-    return indonesia_map
-
-def table_view(indonesia_map=None):
-    st.title('Table Data from Interactive Map')
-
-    if indonesia_map is not None:
-        table_data = indonesia_map[['Propinsi', 'Mock Value']]
-        table_data.rename(columns={'Propinsi': 'Province', 'Mock Value': 'Value'}, inplace=True)
-        st.write(table_data)
-    else:
-        st.write("No data available")
+def html_component():
+    components.html(open("src/frontend/index.html").read())
 
 def main():
-    pages = ["Data View", "Distribution Chart", "Correlation Heatmap", "Interactive Map", "Table View"]
+    if 'indonesia_map' not in st.session_state:
+        st.session_state.indonesia_map = None
+    pages = {
+        "Welcome Page": welcome_page.WelcomePage(),
+        "Data View": data_view.DataView(),
+        "Distribution Chart": distribution_chart.DistributionChart(),
+        "Correlation Heatmap": correlation_heatmap.CorrelationHeatmap(),
+        "Interactive Map": interactive_map.InteractiveMap(),
+        "Table View": table_view.TableView(),
+        "HTML Component": html_component
+    }
 
     with st.sidebar:
-        selected_page = option_menu("Menu Navigasi", pages)
+        selected_page = option_menu("Menu Navigasi", list(pages.keys()))
 
-    if selected_page == "Data View":
-        data_view()
-    elif selected_page == "Distribution Chart":
-        distribution_chart()
-    elif selected_page == "Correlation Heatmap":
-        correlation_heatmap()
-    elif selected_page == "Interactive Map":
-        interactive_map()
-    elif selected_page == "Table View":
-        indonesia_map = interactive_map()
-        table_view(indonesia_map)
+    page = pages[selected_page]
+    page()
 
 if __name__ == "__main__":
     main()
